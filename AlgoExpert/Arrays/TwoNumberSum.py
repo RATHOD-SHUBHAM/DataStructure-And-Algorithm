bb39ea01c3f011a09aeff0df55607d5f50479b1a
shubham <rathsam4@gmail.com>
2021-10-05 14:32:59 -0500

    Two Sum algo expert

--- AlgoExpert/Arrays/TwoNumberSum.py
+++ AlgoExpert/Arrays/TwoNumberSum.py
@@ -2,6 +2,9 @@
 # space complexity = o(n)
 
 def twoNumberSum(array, targetSum):
+
+	if not array or len(array) == 1:
+		return []
     # Write your code here.
 	dicti = {}
 	
@@ -16,4 +19,27 @@ def twoNumberSum(array, targetSum):
 			
 	return [] # if the array has no element or just one element or if there was no pair then return the empty list
 			
+'''
 
+def twoNumberSum(array, targetSum):
+
+	if not array or len(array) == 1:
+		return []
+    # Write your code here.
+	dicti = {}
+	
+	for i in range(len(array)):
+		diff = targetSum - array[i] # calculate diff between target and every other item in the array
+		
+		if diff in dicti:
+			return ([diff, array[i]])
+
+		dicti[array[i]] = diff
+			
+	return [] # if the array has no element or just one element or if there was no pair then return the empty list
+			
+
+
+
+
+'''
