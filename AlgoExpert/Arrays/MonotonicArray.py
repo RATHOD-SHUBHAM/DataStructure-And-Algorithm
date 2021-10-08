ab0baba2de3d61a391c5d71d8dfbdb1e02e2bea0
shubham <rathsam4@gmail.com>
2021-10-07 23:00:46 -0500

    monotonic array

--- AlgoExpert/Arrays/MonotonicArray.py
+++ AlgoExpert/Arrays/MonotonicArray.py
@@ -6,26 +6,26 @@ Rest everything is True
 '''
 def isMonotonic(array):
     increasing = True
-	decresing = True
+	decreasing = True
 	
 	for i in range(1,len(array)):
 		if array[i] > array[i-1]:
-			decresing = False
+			decreasing = False
 		if array[i] < array[i-1]:
 			increasing = False
 			
 	return increasing or decreasing 
 
 '''
-	if any where in between suddenly the value change
-	then one of the flag become true
+if any where in between suddenly the value change
+then one of the flag become true
 
-	But thats fine. one flag can be false and one can be true
+But thats fine. one flag can be false and one can be true
 
 
-	but if both the flag turns false
+but if both the flag turns false
 
-	then that means the value inc or dec (vise versa)
+then that means the value inc or dec (vise versa)
 
 '''
 
