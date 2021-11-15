bf62677a2119014470c199989f923bc30cf24170
shubham <rathsam4@gmail.com>
2021-11-15 00:08:03 -0600

    insertion Sort

---
+++ AlgoExpert/Sorting Algorithm/Insertion Sort/sol.py
@@ -0,0 +1,10 @@
+# Time Complexity = O(n^2)
+# Space Complexity = O(1)
+
+def insertionSort(array):
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j] < array[j-1]:
+            array[j], array[j-1] = array[j-1], array[j]
+            j -= 1
+    return array
