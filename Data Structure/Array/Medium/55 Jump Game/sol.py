cbd3b4418eb38dfebaa2077a907a7c4e4c47b351
shubham <rathsam4@gmail.com>
2021-11-30 23:02:51 -0600

    Jump

---
+++ Data Structure/Array/Medium/55 Jump Game/sol.py
@@ -0,0 +1,42 @@
+'''
+https://www.youtube.com/watch?v=Yan0cv2cLy8
+
+55. Jump Game
+
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+0 <= nums[i] <= 105
+
+'''
+
+# Time: O(n)
+# Space O(1)
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        target = len(nums) - 1
+        
+        for i in reversed(range(len(nums)-1)):
+            if target <= (nums[i] + i):
+                target = i
+                
+        return True if target == 0 else False
