3a87f670ddedc5401012054d80152c60de908ee4
shubham <rathsam4@gmail.com>
2021-12-02 23:14:36 -0600

    Three Sorted Number

---
+++ Data Structure/Searching Algorithm/Problems/Binary Search/sol.py
@@ -0,0 +1,27 @@
+# Binary Search: Split the array into half for each traversal.
+# In binary tree,  Array should always be sorted.
+# time Complexity = O(log(n))
+# Space Complexity = O(1)
+
+def binarySearch(array, target):
+	left = 0
+	right = len(array) - 1
+    return helper (array,target,left,right)
+
+def helper(array,target,left,right):
+	# did not find the match, return -1
+	if left > right:
+		return -1
+	
+	mid = (left + right) //2
+	
+	# found the element so return index
+	if array[mid] == target:
+		return mid
+	elif array[mid] < target:
+		left = mid + 1
+		return helper(array,target,left,right)
+	else:
+		right = mid - 1
+		return helper(array,target,left,right)
+	
