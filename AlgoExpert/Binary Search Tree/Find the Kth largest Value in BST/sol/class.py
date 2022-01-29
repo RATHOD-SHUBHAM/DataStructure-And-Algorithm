ceb83459efbe9d4fa2bd32b9d882a7812dd33a30
shubham <rathsam4@gmail.com>
2022-01-29 00:02:01 -0600

    kth largest number

---
+++ AlgoExpert/Binary Search Tree/Find the Kth largest Value in BST/sol/class.py
@@ -0,0 +1,29 @@
+# Time and space = O(n)
+# This is an input class. Do not edit.
+class BST:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+		
+class TreeInfo:
+	def __init__(self,no_of_nodes_visited,last_visited_node):
+		self.no_of_nodes_visited = no_of_nodes_visited
+		self.last_visited_node = last_visited_node
+
+def findKthLargestValueInBst(tree, k):
+    treeInfo = TreeInfo(0,0)
+	reversedInorder(tree,k,treeInfo)
+	return treeInfo.last_visited_node
+
+def reversedInorder(tree, k ,treeInfo):
+	if not tree or treeInfo.no_of_nodes_visited >= k:
+		return
+	reversedInorder(tree.right, k ,treeInfo)
+	
+	if treeInfo.no_of_nodes_visited < k:
+		treeInfo.no_of_nodes_visited += 1
+		treeInfo.last_visited_node = tree.value
+		reversedInorder(tree.left, k ,treeInfo)
+	
