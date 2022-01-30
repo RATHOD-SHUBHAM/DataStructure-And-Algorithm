ceab388b728e3dea0ab4296c7a6516e404deab04
shubham <rathsam4@gmail.com>
2022-01-30 11:37:42 -0600

    Merge Two Sorted Array

---
+++ Data Structure/LL/MergeTwosortedLL/iterative.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        l1 = list1
+        l2 = list2
+        
+        if not l1 and not l2: return
+        
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1,l2.next)
+            return l2
