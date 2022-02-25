44875a32be78df2d3da8c27baae9fbb3144fcd81
shubham <rathsam4@gmail.com>
2022-02-25 13:28:35 -0600

    K_sum

--- AlgoExpert/Arrays/15. 4sum/K_sum leetcode/Ksum.py
+++ AlgoExpert/Arrays/15. 4sum/K_sum leetcode/Ksum.py
@@ -1,58 +1,52 @@
+# Time = O(n^3) | Space = O(n)
+
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-	
-        def kSum(nums: List[int], target: int, k: int) -> List[List[int]]:
-            res = []
-            print("k : ",k)
-            print("target : ",target)
-            # If we have run out of numbers to add, return res.
-            if not nums:
-                return res
-            print("nums: ",nums)
-            # There are k remaining values to add to the sum. The 
-            # average of these values is at least target // k.
-            average_value = target // k
-            
-            print("average_value : ",average_value)
-            
-            # We cannot obtain a sum of target if the smallest value
-            # in nums is greater than target // k or if the largest 
-            # value in nums is smaller than target // k.
-            if average_value < nums[0] or nums[-1] < average_value:
-                print("res:s ",res)
-                return res
-            
-            if k == 2:
-                return twoSum(nums, target)
-    
-            for i in range(len(nums)):
-                print("nums[i] : ",nums[i])
-                # skip duplicate
-                if i == 0 or nums[i - 1] != nums[i]:
-                    print("hi\n")
-                    for subset in kSum(nums[i + 1:], target - nums[i], k - 1):
-                        print("subset is : ",subset)
-                        print("\n")
-                        res.append([nums[i]] + subset)
-                        print("PreFinal res: ",res)
-    
-            return res
-
-        def twoSum(nums: List[int], target: int) -> List[List[int]]:
-            res = []
-            s = set()
-    
-            for i in range(len(nums)):
-                if len(res) == 0 or res[-1][1] != nums[i]:
-                    if target - nums[i] in s:
-                        res.append([target - nums[i], nums[i]])
-                s.add(nums[i])
-    
-            print("res for k = 2: ",res)
-            return res
-
-        
-        
         nums.sort()
-        return kSum(nums, target, 3)
+        k = 4
+        return self.k_sum(nums,target, k)
+    
+    def k_sum(self, nums, target, k):
+        res = []
+        
+        # base case
+        if not nums:
+            return res
+        
+        # There are k remaining values to add to the sum. The 
+        # average of these values is at least target // k.
+        average_sum = target // k
+        
+        # We cannot obtain a sum of target if the smallest value
+        # in nums is greater than target // k or if the largest 
+        # value in nums is smaller than target // k.
+        if average_sum < nums[0] or nums[-1] < average_sum:
+            return res
+        
+        if k == 2:
+            return self.two_sum(nums, target)
+        
+        # for greater than 2
+        for i in range(len(nums)):
+            # check for duplicates: if duplicates skip
+            if i == 0 or nums[i] != nums[i-1]:
+                # for every value that 2 sum return add it with current number
+                for num in self.k_sum(nums[i+1 : ], target - nums[i], k-1):
+                    res.append([nums[i]] + num)
+                    
+        return res
+    
+    def two_sum(self, nums, target):
+        res = []
+        cache_set = set()
+        
+        for i in range(len(nums)):
+            # check for duplicates
+            if len(res) == 0 or res[-1][1] != nums[i]:
+                if target - nums[i] in cache_set:
+                    res.append([target - nums[i] , nums[i]])
+            
+            cache_set.add(nums[i])
+    
+        return res
         
