f8905fd0146a8f192c6b71c5ee8453cf55384d01
RATHOD-SHUBHAM <rathsam4@gmail.com>
2023-05-23 07:35:29 -0500

    Connected Components

---
+++ Striver/Graph Series/BFS:DFS/323. Number of Connected Components in an Undirected Graph/sol.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        visited = [False] * n
+        
+        # Get neighbor ------------------------
+        
+        dic = defaultdict(list)
+        for i in range(len(edges)):
+            parent, child = edges[i]
+            dic[parent].append(child)
+            dic[child].append(parent)
+        
+        # Helper Code ------------------------
+        
+        def dfs(i):
+            if visited[i] == True:
+                return
+            
+            visited[i] = True
+            
+            # check if the node has any edge
+            if i in dic:
+                for child in dic[i]:
+                    if visited[child] == False:
+                        dfs(child)
+
+            return
+        
+        
+        # Main Code ------------------------
+        
+        connectedComponents = 0
+        
+        for i in range(n):
+            # if there is a edge
+            if i in dic:
+                if visited[i] == False:
+                    dfs(i)
+                    connectedComponents += 1
+                
+            # if there is no edge - single node
+            else:
+                if visited[i] == False:
+                    connectedComponents += 1
+        
+        return connectedComponents
