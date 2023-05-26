cf85a38091a0a246e3d8fa8db6e9d61ec5f174eb
RATHOD-SHUBHAM <rathsam4@gmail.com>
2023-05-25 19:57:18 -0500

    new test case

--- Striver/Graph Series/BFS:DFS/Detect cycle in an undirected graph/DFS/sol.py
+++ Striver/Graph Series/BFS:DFS/Detect cycle in an undirected graph/DFS/sol.py
@@ -9,30 +9,30 @@ class Solution:
 		parent = -1
 		
 		for i in range(V):
-		    if visited[i] == False:
-		        isCyclePresent = self.dfs(i, parent, visited, V, adj)
-		        
-		        if isCyclePresent:
-		            return 1
+			if visited[i] == False:
+				isCyclePresent = self.dfs(i, parent, visited, V, adj)
+				
+				if isCyclePresent:
+					return 1
 
-        return 0
-        
-        
-    def dfs(self, i, parent, visited, V, adj):
-        
-        visited[i] = True
-        
-        for child in adj[i]:
-            if visited[child] == False:
-                isCyclePresent = self.dfs(child, i, visited, V, adj)
-                
-                if isCyclePresent:
-		            return True
-            else:
-                if child != parent:
-                    return True
-                    
-        return False
+		return 0
+
+
+	def dfs(self, i, parent, visited, V, adj):
+
+		visited[i] = True
+
+		for child in adj[i]:
+			if visited[child] == False:
+				isCyclePresent = self.dfs(child, i, visited, V, adj)
+				
+				if isCyclePresent:
+					return True
+			else:
+				if child != parent:
+					return True
+					
+		return False
 		    
 
 
