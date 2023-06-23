a0a247137415e642d75945bace0942ec698e55ad
RATHOD-SHUBHAM <rathsam4@gmail.com>
2023-06-23 07:23:59 -0500

    delete in a LlinkedList

--- Striver/LinkedList/1D LL/Operations/Delete/delete.py
+++ Striver/LinkedList/1D LL/Operations/Delete/delete.py
@@ -34,4 +34,74 @@ class LinkedList:
 
         # delete the last
         del(lst_node)
+    
+    def del_Key(self, key):
+        if self.head == None:
+            return
+        
+        curNode = self.head
+        prevNode = None
+
+        while curNode != None:
+            if curNode.data == key:
+
+                # check if the head node is a key element
+                if curNode == self.head:
+                    # move the head to the front
+                    self.head = self.head.next
+                    curNode = self.head
+                else:
+                    prev.next = curNode.next
+                    curNode = curNode.next
+            
+            else:
+                prev = curNode
+                curNode = curNode.next
+
+        return self.head
+    
+
+    def del_position(self, position):
+        if self.head == None:
+            return
+        
+        curNode = self.head
+        prevNode = None
+        curPos = 0
+
+        # check if position is a negative value
+        if position < curPos:
+            print("Posiion is not valid")
+            return
+
+        # if postion == 0
+        if position == curPos:
+            self.head = self.head.next
+            return
+        
+        while curNode != None and curPos < position:
+            prevNode = curNode
+            curNode = curNode.next
+            curPos += 1
+
+        # if the postion is larger than linkedlist
+        if curPos < position:
+            print("Node not available")
+            
+        # we are at the position
+        elif curPos == position:
+            prevNode.next = curNode.next
+            del(curNode)
+            return
+
+        
+
+
+        
+        
+
+        
+        
+
+        
         
