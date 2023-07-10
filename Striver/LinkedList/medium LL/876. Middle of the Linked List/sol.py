9d315b307f082c0bf2b916a38de3120ff9cce016
RATHOD-SHUBHAM <rathsam4@gmail.com>
2023-07-10 08:30:52 -0400

    middle of LL

--- Striver/LinkedList/medium LL/876. Middle of the Linked List/sol.py
+++ Striver/LinkedList/medium LL/876. Middle of the Linked List/sol.py
@@ -1,6 +1,10 @@
 # Tc: O(n) | Sc: O(1)
 
 # Definition for singly-linked list.
+'''
+1->2->3->4
+op = 3
+'''
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -10,6 +14,28 @@ class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         tortise = head
         heir = head
+        
+        while heir and heir.next:
+            tortise = tortise.next
+            heir = heir.next.next
+        
+        return tortise
+    
+# --------------------------------------------------------------------
+# Definition for singly-linked list.
+'''
+1->2->3->4
+op = 2
+'''
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        tortise = head
+        heir = head.next
         
         while heir and heir.next:
             tortise = tortise.next
