c86fe7e589b444ebaa2b0cfa3d4578ca872af459
RATHOD-SHUBHAM <rathsam4@gmail.com>
2023-10-01 23:19:42 -0400

    Valid parenthesis

---
+++ Blind75/stack/Valid Parentheses/Sol/stack.py
@@ -0,0 +1,30 @@
+# Tc: O(n) | Sc: O(n)
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {
+            ')' : '(',
+            ']' : '[',
+            '}' : '{'
+        }
+
+        stack = [] # to get the order from string
+
+        for i in s:
+            # if opening bracket , add to stack
+            if i == '(' or i == '[' or i == '{':
+                stack.append(i) # Sc: O(n)
+            else:
+                # if the first element itself is closing bracket
+                if not stack:
+                    return False
+
+                brac = stack.pop()
+
+                # Compare if the bracket matches
+                if dic[i] != brac:
+                    return False
+        
+        return True if stack == [] else False
+
+
