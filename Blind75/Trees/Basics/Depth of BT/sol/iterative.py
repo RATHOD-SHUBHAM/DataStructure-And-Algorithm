371f3feb4f5cac9440da11f0580a5286f8632cc6
RATHOD-SHUBHAM <rathsam4@gmail.com>
2023-10-10 08:46:07 -0400

    depth of BT

---
+++ Blind75/Trees/Basics/Depth of BT/sol/iterative.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        max_depth = 0     
+        cur_depth = 1
+
+        stack = []
+        stack.append([cur_depth, root])
+
+        while stack:
+            cur_depth , node = stack.pop()
+
+            max_depth = max(cur_depth, max_depth)
+
+            if node.left:
+                stack.append([cur_depth + 1, node.left])
+            
+            if node.right:
+                stack.append([cur_depth + 1, node.right])
+
+        return max_depth
