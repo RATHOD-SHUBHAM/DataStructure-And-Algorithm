1d2a3d37c7010a098e094565e527d179db17890a
RATHOD-SHUBHAM <rathsam4@gmail.com>
2023-10-14 16:33:49 -0400

    TREE COMPLETE

---
+++ Blind75/Trees/297. Serialize and Deserialize Binary Tree/sol.py
@@ -0,0 +1,70 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+
+        # return type is a string
+        string = ""
+        string = self.DFS(root, string)
+
+        # print(string)
+        return string
+    
+    def DFS(self, root, string):
+        if not root:
+            string += 'None,' # careful if we add space, then while desrializing we will have to take care of None as well as (space)None
+        else:
+            string += str(root.val) + ','
+            string = self.DFS(root.left, string)
+            string = self.DFS(root.right, string)
+
+        return string
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        # print(data)
+        data_list = data.split(',')
+        # print(data_list)
+
+        root = self.constructTree(data_list)
+
+        return root
+    
+    def constructTree(self, data_list): 
+        
+        root_val = data_list.pop(0)
+
+        if root_val == 'None':
+            return
+
+
+        root = TreeNode(root_val)
+
+        root.left = self.constructTree(data_list)
+        root.right = self.constructTree(data_list)
+
+        return root
+
+        
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
