225ecdb168d66d45d09f14d69d03b15c830bc23f
RATHOD-SHUBHAM <rathsam4@gmail.com>
2023-11-06 20:04:36 -0500

    802. Find Eventual Safe States

--- Striver/Graph Series/Topo Sort/802. Find Eventual Safe States/kahns.py
+++ Striver/Graph Series/Topo Sort/802. Find Eventual Safe States/kahns.py
@@ -1,44 +1,49 @@
-from collections import defaultdict
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         n = len(graph)
-        
+
+        # get the neighbors
+        adj_nei = collections.defaultdict(list)
+
+        for i in range(n):
+            edges = graph[i]
+
+            for edge in edges:
+                adj_nei[edge].append(i)
+
+        # get indegree
         outdegree = [0] * n
+
         for i in range(n):
-            outdegree[i] = len(graph[i])
+            edges = graph[i]
+
+            outdegree[i] = len(edges)
         
-        # print(outdegree)
         
-        adj_nei = defaultdict(list)
-        for i in range(n):
-            for nei in graph[i]:
-                adj_nei[nei].append(i)
-        
-        # print(adj_nei)
-        
-        # get the node with outdegree 0
+        # add to queue
         queue = []
         for i in range(n):
             if outdegree[i] == 0:
-                queue.append(i)
-        # print(queue)
-        
-        safe_node = [False] * n
+                queue.append(i)   
+
+
+        # topo sort
+        topo_sort = []
+        count = 0
+
         while queue:
             node = queue.pop(0)
-            safe_node[node] = True
             
+
             for nei in adj_nei[node]:
                 outdegree[nei] -= 1
-                
+
                 if outdegree[nei] == 0:
                     queue.append(nei)
+            
+            topo_sort.append(node)
+            count += 1
+
         
-        # print(safe_node)
-        
-        safeNode = []
-        for i in range(n):
-            if safe_node[i] == True:
-                safeNode.append(i)
-        
-        return safeNode
+        topo_sort.sort()
+        return topo_sort
