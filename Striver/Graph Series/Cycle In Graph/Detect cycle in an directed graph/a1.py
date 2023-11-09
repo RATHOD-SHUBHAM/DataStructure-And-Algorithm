76a781cdad7b2cdb9420152f2e5396783e55e2f1
RATHOD-SHUBHAM <rathsam4@gmail.com>
2023-11-08 22:02:37 -0500

    detect path in directed graph

--- Striver/Graph Series/Cycle In Graph/Detect cycle in an directed graph/a1.py
+++ Striver/Graph Series/Cycle In Graph/Detect cycle in an directed graph/a1.py
@@ -1,3 +1,5 @@
+#  ------ ------ Track Path algorithm ------ ------ ------ ------
+
 # TC: O(V + E) | SC : O(V)
 def cycleInGraph(edges):
     n = len(edges)
@@ -39,3 +41,43 @@ def detectCycle(node, edges, visited, track_path):
     # remove node from path
     track_path[node] = False
     return False
+
+
+#  ------ ------ Kahns algorithm ------ ------ ------ ------
+# Tc: O(v+e) | Sc: O(n)
+
+def cycleInGraph(edges):
+    n = len(edges)
+    
+    indegree = [0] * n
+
+    # get the indegree of each edge
+    for i in range(n):
+        for nei in edges[i]:
+            indegree[nei] += 1
+
+    # get the node with indegree 0
+    queue = []
+    for i in range(n):
+        if indegree[i] == 0:
+            queue.append(i)
+
+    # reduce the indegree
+    count = 0
+    topo_sort = []
+    while queue:
+        node = queue.pop(0)
+
+        for nei in edges[node]:
+            indegree[nei] -= 1
+
+            if indegree[nei] == 0:
+                queue.append(nei)
+
+        count += 1
+        topo_sort.append(node)
+
+    if count != n:
+        return True
+    else:
+        return False
