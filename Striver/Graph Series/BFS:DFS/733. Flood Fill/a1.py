11a7062b042a8a1415197c9d89dfffb481cb8f51
RATHOD-SHUBHAM <rathsam4@gmail.com>
2023-11-08 20:54:04 -0500

    flood fill

--- Striver/Graph Series/BFS:DFS/733. Flood Fill/a1.py
+++ Striver/Graph Series/BFS:DFS/733. Flood Fill/a1.py
@@ -1,3 +1,41 @@
+# BFS
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        m = len(image)
+        n = len(image[0])
+
+        # If the starting cell is already colored
+        if image[sr][sc] == color:
+            return image
+
+        directions = [(-1, 0), (1,0), (0,-1), (0,1)]
+        
+        queue = [[sr, sc]]
+
+        start_color = image[sr][sc]
+
+        while queue:
+            row, col = queue.pop(0)
+
+            # Color the cell
+            image[row][col] = color
+
+            for adj_row, adj_col in directions:
+                nei_row = adj_row + row
+                nei_col = adj_col + col
+
+                if nei_row < 0 or nei_col < 0 or nei_row >= m or nei_col >= n :
+                    continue
+                
+                queue.append([nei_row, nei_col])
+
+        return image
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# DFS
+
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         m = len(image)
--- Striver/Graph Series/BFS:DFS/733. Flood Fill/bfs.py
+++ Striver/Graph Series/BFS:DFS/733. Flood Fill/bfs.py
@@ -2,39 +2,30 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         m = len(image)
         n = len(image[0])
+
+        # If the starting cell is already colored
+        if image[sr][sc] == color:
+            return image
+
+        directions = [(-1, 0), (1,0), (0,-1), (0,1)]
         
-        # create a duplicate for the input image
-        dup_image = [[None for _ in range(n)] for _ in range(m)]
-        for i in range(m):
-            for j in range(n):
-                dup_image[i][j] = image[i][j]
-        
-        # change the given cell color
-        dup_image[sr][sc] = color
-        
-        # BFS
-        queue = [(sr,sc)]
-        
-        directions = [(-1,0), (1,0), (0,-1), (0,1)]
-        
+        queue = [[sr, sc]]
+
+        start_color = image[sr][sc]
+
         while queue:
-            r, c = queue.pop(0)
-            
-            for direction in directions:
-                nr , nc = direction
-                
-                nei_r = nr + r
-                nei_c = nc + c
-                
-                if ( nei_r < 0 or nei_c < 0 or nei_r >= m or nei_c >= n or 
-                    dup_image[nei_r][nei_c] != image[sr][sc] or
-                    dup_image[nei_r][nei_c] == color
-                   ):
+            row, col = queue.pop(0)
+
+            # Color the cell
+            image[row][col] = color
+
+            for adj_row, adj_col in directions:
+                nei_row = adj_row + row
+                nei_col = adj_col + col
+
+                if nei_row < 0 or nei_col < 0 or nei_row >= m or nei_col >= n or image[nei_row][nei_col] != start_color or image[nei_row][nei_col] == color:
                     continue
                 
-                # change the color
-                dup_image[nei_r][nei_c] = color
-                queue.append((nei_r, nei_c))
-        
+                queue.append([nei_row, nei_col])
 
-        return dup_image
+        return image
