7f7a55a4320f768b8e997cd4e1808f509bf440f2
RATHOD-SHUBHAM <rathsam4@gmail.com>
2023-11-11 19:21:41 -0500

    153. Find Minimum in Rotated Sorted Array

--- Blind75/Binary Search/153. Find Minimum in Rotated Sorted Array/binary.py
+++ Blind75/Binary Search/153. Find Minimum in Rotated Sorted Array/binary.py
@@ -16,45 +16,25 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         n = len(nums)
 
-        # base condition
-        # if there is only one element in array
-        if n == 1:
-            return nums[0]
-
-        # if the array is not rotated
-        if nums[0] < nums[-1]:
-            return nums[0]
-
         left = 0
         right = n - 1
 
-        min_array = self.binarySearch(left, right, nums, n)
-
-        return min_array
-    
-    def binarySearch(self, left, right, nums, n):
-        '''
-        * We need to find 'Inflation Point'
-
-        Stopping condition
-            * nums[mid] > nums[mid + 1] Hence, mid+1 is the smallest.
-
-            * nums[mid - 1] > nums[mid] Hence, mid is the smallest.
-        '''
+        # Check if the array is already sorted
+        if nums[left] < nums[right]:
+            return nums[left]
 
         while left < right:
-            # Inflation point
             mid = left + (right - left) // 2
 
-            if nums[mid + 1] < nums[mid]:
-                return nums[mid + 1]
-            
-            if nums[mid - 1] > nums[mid]:
-                return nums[mid]
-
-            if nums[mid] > nums[left]:
-                # all element in between left to mid are greater
-                left = mid + 1
-            
+            if nums[mid] > nums[right]:
+                if nums[mid] > nums[mid + 1]:
+                    return nums[mid + 1]
+                else:
+                    left = mid + 1
             else:
-                right = mid - 1
+                if nums[mid] > nums[left]:
+                    return nums[left]
+                else:
+                    right = mid
+        
+        return nums[left]
