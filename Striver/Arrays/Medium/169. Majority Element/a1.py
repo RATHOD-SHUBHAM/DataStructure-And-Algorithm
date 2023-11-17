c0c77ff21e688fbad46dbdd812347e27e6a0ea95
RATHOD-SHUBHAM <rathsam4@gmail.com>
2023-11-17 08:57:28 -0500

    boyer moore

---
+++ Striver/Arrays/Medium/169. Majority Element/a1.py
@@ -0,0 +1,58 @@
+# Mode calculation
+
+# Tc and Sc: O(n)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        dic = {}
+
+        n = len(nums)
+
+        for i in range(n):
+            cur_num = nums[i]
+
+            if cur_num in dic:
+                dic[cur_num] += 1
+            else:
+                dic[cur_num] = 1
+        
+        return max(dic)
+        
+
+# ------------------------------------------------------------
+
+# Boyer-Moore Voting Algorithm
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        count = 0
+        majority_ele = None
+
+        for i in range(n):
+            if count == 0:
+                majority_ele = nums[i]
+                count += 1
+            
+            elif nums[i] == majority_ele:
+                count += 1
+            else:
+                count -= 1
+        
+        # You may assume that the majority element always exists in the array.
+        return majority_ele
+
+        '''
+        # if there is no gurantee that majority element always exists then
+        
+        # check if this is the majority element
+        count_of_majority_ele = 0
+        for i in range(n):
+            if majority_ele == nums[i]:
+                count_of_majority_ele += 1
+        
+        if count_of_majority_ele > (n/2):
+            return majority_ele
+        else:
+            return -1
+        '''
