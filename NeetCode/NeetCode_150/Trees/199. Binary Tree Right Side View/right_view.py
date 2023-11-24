c433421cd52dd13ab00b7db7ee1c97c7b11b9227
RATHOD-SHUBHAM <rathsam4@gmail.com>
2023-11-23 20:02:05 -0500

    199. Binary Tree Right Side View

---
+++ NeetCode/NeetCode_150/Trees/199. Binary Tree Right Side View/right_view.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return root
+
+        queue = [root]
+
+        op = []
+
+        while queue:
+            q_len = len(queue)
+
+            for i in range(q_len):
+                node = queue.pop(0)
+
+                if node.left:
+                    queue.append(node.left)
+                
+                if node.right:
+                    queue.append(node.right)
+                
+
+                if i == q_len - 1:
+                    op.append(node.val)
+        
+        return op
