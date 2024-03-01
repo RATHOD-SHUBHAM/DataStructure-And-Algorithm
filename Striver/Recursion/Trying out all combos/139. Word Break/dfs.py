4603e5718a111bc03dc46661cea6cb0920cf89b8
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-03-01 07:22:39 -0500

    cache

--- Striver/Recursion/Trying out all combos/139. Word Break/dfs.py
+++ Striver/Recursion/Trying out all combos/139. Word Break/dfs.py
@@ -31,5 +31,37 @@ class Solution:
         return False
 
         
+# Solution 2: Same method
 
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)
 
+        memo = {} # keep track of all the substring - valid and invalid
+
+        valid = self.memoization(memo, wordSet, s)
+        memo[s] = valid
+
+        return memo[s]
+    
+    def memoization(self, memo, wordSet, s):
+        # if the substring is present in cache
+        if s in memo:
+            return memo[s]
+        
+        if s in wordSet:
+            memo[s] = True
+            return True
+        
+        for end in range(len(s) + 1):
+            cur_substring = s[ : end]
+
+            # check if the current substring and the remaining substring are valid
+            if cur_substring in wordSet and self.memoization(memo, wordSet, s[end : ]):
+                memo[cur_substring] = True
+                return True
+            
+        
+        memo[s] = False
+        
+        return False
