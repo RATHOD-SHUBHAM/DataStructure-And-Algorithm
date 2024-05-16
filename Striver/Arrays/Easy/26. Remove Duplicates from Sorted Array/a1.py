5e00d92386d065e41f979974ca1515120e3087d0
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-05-16 07:16:42 -0400

    3 pointer

--- Striver/Arrays/Easy/26. Remove Duplicates from Sorted Array/a1.py
+++ Striver/Arrays/Easy/26. Remove Duplicates from Sorted Array/a1.py
@@ -30,4 +30,30 @@ class Solution:
         
         # print(insertPosition)
         return insertPosition
+
+# ------------------------ 3 pointer -------------------------
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        i = 0
+        j = i
+        k = 1
+
+        while j < n:
+            if nums[i] == nums[j]:
+                j += 1
+            else:
+                self.swap(k, j, nums)
+                i = k
+                k += 1
+                j += 1
         
+        # print(i)
+        # print(nums)
+
+        return i + 1
+    
+    def swap(self, k, j ,nums):
+        nums[k] , nums[j] = nums[j], nums[k]
+        return
