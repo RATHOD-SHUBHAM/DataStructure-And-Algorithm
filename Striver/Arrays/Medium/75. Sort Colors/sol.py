d0cfb1f5b06ca7655ba22eeb7bcd2769d453a18b
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-05-19 21:47:37 -0400

    Dutch national land problem

--- Striver/Arrays/Medium/75. Sort Colors/sol.py
+++ Striver/Arrays/Medium/75. Sort Colors/sol.py
@@ -1,4 +1,6 @@
 '''
+# Dutch national land problem.
+
 # The problem is known as Dutch National Flag Problem
 
 Three pointer is needed:
@@ -10,6 +12,10 @@ Place the pointers:
     1. Put all the 2 to the end and move the right pointer.
     2. Put all the zero at zero pointer and then move the zero pointer
     3. When there is one - Just proceed since the above two pointer will take care of one
+
+Summary:
+    1. p0 -> Places all the 0s in the right position.
+    2. right -> Places all the 1s in the right position.
 '''
 
 class Solution:
@@ -17,26 +23,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums) 
+        n = len(nums)
 
-        zero_pointer = 0
+        p0 = 0 # keep track of 0
         left = 0
-        right = n - 1
-        
+        right = n - 1 # Keep track of 2
+
         while left <= right:
-            cur_num = nums[left]
-
-            if cur_num == 0:
-                # put it at a position where zero pointer will be pointing
-                nums[zero_pointer] , nums[left] = nums[left] , nums[zero_pointer]
-                zero_pointer += 1
-                left += 1
-            
-            elif cur_num == 2:
-                # place the 2 at correct position
-                nums[left] , nums[right] = nums[right] , nums[left]
+            if nums[left] == 2:
+                val = self.swap(left, right, nums)
                 right -= 1
-
+            elif nums[left] == 0:
+                self.swap(left, p0, nums)
+                p0 += 1
+                left += 1
             else:
                 left += 1
         
