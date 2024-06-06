6102fadb354cccbe266a7f50624b2e500c76df61
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-06-06 06:57:41 -0400

    1299. Replace Elements with Greatest Element on Right Side

--- Striver/Arrays/Medium/Leader in Array/1299. Replace Elements with Greatest Element on Right Side/a1.py
+++ Striver/Arrays/Medium/Leader in Array/1299. Replace Elements with Greatest Element on Right Side/a1.py
@@ -1,3 +1,21 @@
+# Tc: TLE | Sc: O(1)
+
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        n = len(arr)
+
+        for i in range(n):
+            if i == n - 1:
+                arr[i] = -1
+                continue
+
+            max_value = max(arr[i+1 : ])
+            arr[i] = max_value
+        
+        return arr
+    
+# ------------ Reverse the array and find the greatest ---------------
+
 # Tc: O(n) | Sc: O(1)
 
 class Solution:
