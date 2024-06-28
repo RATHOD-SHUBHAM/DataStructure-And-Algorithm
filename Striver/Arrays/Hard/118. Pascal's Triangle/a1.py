49fa1853d1b31cd779c88d1f1d7a14bd9227d312
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-06-28 08:09:11 -0400

    118. Pascal's Triangle

--- Striver/Arrays/Hard/118. Pascal's Triangle/a1.py
+++ Striver/Arrays/Hard/118. Pascal's Triangle/a1.py
@@ -117,3 +117,23 @@ class Solution:
         
         return op
 
+# -----------------  Final -------------- 
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        final_op = [[1]] # BASE CASE - by default we should have [1]
+
+        for i in range(1, numRows):
+            arr = final_op[-1]
+            cur_list = []
+
+
+            for i in range(1, len(arr)):
+                cur_sum = arr[i-1] + arr[i] # current number is always sum of current and previous index
+                cur_list.append(cur_sum)
+            
+            new_list = [1] + cur_list + [1]
+
+            final_op.append(new_list)
+        
+        return final_op
---
+++ Striver/Arrays/Hard/118. Pascal's Triangle/final.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        final_op = [[1]] # BASE CASE - by default we should have [1]
+
+        for i in range(1, numRows):
+            arr = final_op[-1]
+            cur_list = []
+
+
+            for i in range(1, len(arr)):
+                cur_sum = arr[i-1] + arr[i] # current number is always sum of current and previous index
+                cur_list.append(cur_sum)
+            
+            new_list = [1] + cur_list + [1]
+
+            final_op.append(new_list)
+        
+        return final_op
