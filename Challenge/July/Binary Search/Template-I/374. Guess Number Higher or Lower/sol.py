2d9dcb05c67c0bf98de14b2e652cc72f5b62e05d
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-07-03 08:47:58 -0400

    challenge day 03

---
+++ Challenge/July/Binary Search/Template-I/374. Guess Number Higher or Lower/sol.py
@@ -0,0 +1,23 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        low = 1
+        high = n
+
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            res = guess(mid)
+
+            if res == 0:
+                return mid
+            elif res == 1:
+                low = mid + 1
+            else:
+                high = mid - 1
