1ac65ee7580d2e7017f34840128380cec0a5513a
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-07-13 21:02:08 -0400

    Three number sort

--- Striver/Algorithms/Sorting/Three Number Sort/ThreeNumberSort/dutchLand.py
+++ Striver/Algorithms/Sorting/Three Number Sort/ThreeNumberSort/dutchLand.py
@@ -1,33 +1,34 @@
 # Tc: O(n) | Sc: O(1)
 
-# DutchLand Problem
+# DutchLand Sorting Problem
 def threeNumberSort(array, order):
-    
     first_ele = order[0]
-    last_ele = order[-1]
+    last_ele = order[2]
 
     n = len(array)
-    
-    # Pointers
-    p0 = 0
-    left = 0
-    right = n - 1
 
-    while left <= right:
-        
-        if array[left] == last_ele:
-            
-            array[left] , array[right] = array[right] , array[left]
+    left = 0 # keep track of first/left element
+    p = 0
+    right = n - 1 # keep track of last/right element
+
+    while p <= right:
+        if array[p] == first_ele:
+            '''
+                Placing the first element to the left
+            '''
+            swap(left, p , array)
+            left += 1
+            p += 1
+        elif array[p] == last_ele:
+            '''
+                Placing the last element to the right
+            '''
+            swap(right, p, array)
             right -= 1
-        
-        elif array[left] == first_ele:
-
-            array[left] , array[p0] = array[p0], array[left]
-            left += 1
-            p0 += 1
-
         else:
-            left += 1
-
+            p += 1
 
     return array
+
+def swap(x , y, array):
+    array[x], array[y] = array[y], array[x]
