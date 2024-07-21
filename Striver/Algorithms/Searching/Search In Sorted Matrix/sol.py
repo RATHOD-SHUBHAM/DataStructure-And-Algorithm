db019ac3a1dd189a3edd2a999c042dd4e6b12355
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-07-21 09:16:38 -0400

    searchInSortedMatrix

--- Striver/Algorithms/Searching/Search In Sorted Matrix/sol.py
+++ Striver/Algorithms/Searching/Search In Sorted Matrix/sol.py
@@ -1,18 +1,25 @@
-# Time Complexity = O(n+m)
-# n = no of row
-# m = no of column
-# space complexity = O(1)
+'''
+    Time Complexity = O(n+m)
+    Space complexity = O(1)
+
+    Start from last column and move diagonally downwards, this way we eliminate searching in zone which are smaller than the desired value
+'''
 
 def searchInSortedMatrix(matrix, target):
+    m = len(matrix)
+    n = len(matrix[0]) - 1
+    
     row = 0
-	col = len(matrix[0]) - 1
-	
-	while row < len(matrix) and col >= 0 :
-		if matrix[row][col] == target:
-			return([row,col])
-		elif matrix[row][col] > target:
-			col -= 1
-		elif matrix[row][col] < target:
-			row += 1
-			
-	return([-1,-1])
+    col = n
+
+    while row < m and col >= 0:
+        cur_val = matrix[row][col]
+
+        if cur_val == target:
+            return [row, col]
+        elif cur_val > target:
+            col -= 1
+        else:
+            row += 1
+
+    return [-1, -1]
