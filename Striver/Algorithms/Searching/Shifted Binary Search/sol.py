36cb60e6021f79855782482124c3266988bb739a
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-07-21 09:05:43 -0400

    33. Search in Rotated Sorted Array

--- Striver/Algorithms/Searching/Shifted Binary Search/sol.py
+++ Striver/Algorithms/Searching/Shifted Binary Search/sol.py
@@ -1,35 +1,26 @@
-# Time Complexity = O(log(n))
-# Space Complexity = O(1)
-
-
-def shiftedBinarySearch(array, target):
-    l = 0
-	r = len(array) - 1
-	
-	while l <= r:
-		# calculate mid point
-		mid = (l + r) // 2
-		
-		if target == array[mid]:
-			return mid
-		
-		# Assure my elements from are in sorted form from left to middle
-		elif array[l] <= array[mid]:
-			# my element is between left and mid
-			if target >= array[l] and target < array[mid]:
-				r = mid - 1
-			else: # my element is not between left and mid
-				l = mid + 1
-				
-		# if the elements are not in sorted order from left to mid 
-		else: # this also means that elements are sorted from mid to right
-			if target > array[mid] and target <= array[r]:
-				l = mid + 1
-			else:
-				r = mid - 1
-				
-	return -1
-			
-		
-			
-		
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        
+        left = 0
+        right = n - 1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            
+            if target == nums[mid]:
+                return mid
+            elif nums[mid] > nums[right]:
+                # left side is sorted
+                if target < nums[mid] and target >= nums[left]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                # right side is sorted
+                if target > nums[mid] and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
+        return -1
