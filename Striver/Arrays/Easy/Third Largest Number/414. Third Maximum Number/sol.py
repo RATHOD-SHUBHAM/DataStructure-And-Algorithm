98c00f42069c271f27ff73fe80b60394039fb6f9
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-08-01 08:11:57 -0400

    Third largest

---
+++ Striver/Arrays/Easy/Third Largest Number/414. Third Maximum Number/sol.py
@@ -0,0 +1,28 @@
+# Follow up to Second largest problem
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        largest = max(nums)
+        # There are negative values
+        second_largest = -math.inf
+        third_largest = -math.inf
+
+        for i in range(n):
+            cur_ele = nums[i]
+
+            if cur_ele == largest:
+                continue
+
+            if cur_ele > second_largest:
+                third_largest = second_largest
+                second_largest = cur_ele
+
+            elif cur_ele < second_largest and cur_ele > third_largest:
+                third_largest = cur_ele
+        
+
+        if third_largest == -math.inf:
+            return largest
+        else:
+            return third_largest
