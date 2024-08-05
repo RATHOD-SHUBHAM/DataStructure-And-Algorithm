fb1481f30b1b7bc446777498bcc6549c528f1bac
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-08-04 22:38:14 -0400

    spiral matrix

--- Striver/Arrays/Medium/54. Spiral Matrix/sol.py
+++ Striver/Arrays/Medium/54. Spiral Matrix/sol.py
@@ -1,57 +1,5 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        left = 0
-        right = len(matrix[0])
-
-        top = 0
-        bottom = len(matrix)
-
-        op = []
-
-        while top < bottom and left < right:
-            # top - left to right
-            for i in range(left, right):
-                op.append(matrix[top][i])
-            
-            top += 1
-
-            if top == bottom:
-                break
-            
-            # right - top to bottom
-            for i in range(top, bottom):
-                op.append(matrix[i][right - 1])
-
-            right -= 1
-
-            if right == left:
-                break
-            
-            # bottom - right to left
-            for i in reversed(range(left, right)):
-                op.append(matrix[bottom - 1][i])
-            
-            bottom -= 1
-
-            if top == bottom:
-                break
-            
-            # left - bottom to top
-            for i in reversed(range(top, bottom)):
-                op.append(matrix[i][left])
-            
-            left += 1
-
-            if left == right:
-                break
-        
-        return op
-    
-
-# --------------  Same solution - different boundaries. --------------
-
-class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         m = len(matrix)
         n = len(matrix[0])
 
@@ -80,6 +28,7 @@ class Solution:
 
             if right < left:
                 break
+
             
             # 3] bottom: move from right to left
             for i in reversed(range(left, right + 1)):
@@ -87,8 +36,9 @@ class Solution:
             
             bottom -= 1
 
-            if bottom < top:
-                break
+            # if bottom < top:
+            #     break
+
             
             # 4] left: move from bottom to top
             for i in reversed(range(top, bottom + 1)):
@@ -96,7 +46,7 @@ class Solution:
             
             left += 1
 
-            if right < left:
-                break
+            # if right < left:
+            #     break
         
         return op
