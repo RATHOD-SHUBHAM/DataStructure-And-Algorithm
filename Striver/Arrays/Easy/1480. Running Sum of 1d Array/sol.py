a98a57ba1f897269cb69272104e975fc6e7fba1b
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-08-06 21:26:18 -0400

    running sum

--- Striver/Arrays/Easy/1480. Running Sum of 1d Array/sol.py
+++ Striver/Arrays/Easy/1480. Running Sum of 1d Array/sol.py
@@ -1,10 +1,12 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        running_sum = [0] * n
-        running_sum[0] = nums[0]
 
-        for i in range(1, n):
-            running_sum[i] = nums[i] + running_sum[i-1]
-        
-        return running_sum
+        op = [None] * n
+
+        cur_sum = 0
+
+        for i in range(n):
+            cur_sum += nums[i]
+            op[i] = cur_sum
+        return op
