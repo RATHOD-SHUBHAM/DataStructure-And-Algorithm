d6f122a8b56360294123ffd8bc1dad50927e8004
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-09-04 20:35:58 -0400

    874. Walking Robot Simulation

---
+++ LeetCode/Daily_Challenege/September_2024/874. Walking Robot Simulation/sol.py
@@ -0,0 +1,45 @@
+class Solution:
+    def squared_euclidean_dist(self, x:int, y:int) -> int:
+        x = (x - 0) ** 2
+        y = (y - 0) ** 2
+        return x + y
+
+    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+        # Store obstacles in an set for efficient lookup
+        obstacle_set = set()
+        for obs in obstacles:
+            obstacle_set.add(tuple(obs))
+
+        # Define direction vectors: North, East, South, West
+        # 0: North, 1: East, 2: South, 3: West
+        directions = [(0, 1), (1, 0), (0, -1),  (-1, 0)]
+        
+        current_direction = 0 
+        x, y = 0, 0
+        max_distance_squared = 0
+        
+        for command in commands:
+            if command == -1:  # Turn right
+                current_direction = (current_direction + 1) % 4
+
+            elif command == -2:  # Turn left
+                current_direction = (current_direction - 1) % 4
+                # current_direction = (current_direction + 3) % 4
+            
+            else:
+
+                # Move forward
+                dx, dy = directions[current_direction]
+
+                # Move Step by Step
+                for _ in range(command):
+                    next_x, next_y = x + dx, y + dy
+                    
+                    if (next_x, next_y) in obstacle_set:
+                        break
+                    
+                    x, y = next_x, next_y
+
+                max_distance_squared = max(max_distance_squared, self.squared_euclidean_dist(x,y))
+
+        return max_distance_squared
