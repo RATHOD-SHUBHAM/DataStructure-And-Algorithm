197db285823c0bc8b0588f573928bc6290596d49
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-09-07 08:03:09 -0400

    1367. Linked List in Binary Tree

---
+++ LeetCode/Daily_Challenege/September_2024/7-1367. Linked List in Binary Tree/sol.py
@@ -0,0 +1,54 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+        if not root:
+            return False
+        
+        cur_ll_node = head
+        node = root
+
+        return self.preOrderTraversal(node, cur_ll_node)
+    
+    def preOrderTraversal(self, node, cur_ll_node):
+        if not node:
+            return False
+
+        if self.validatePath(node, cur_ll_node):
+            # Found a valid path from here
+            return True
+        
+        leftPath = self.preOrderTraversal(node.left, cur_ll_node)
+        rightPath = self.preOrderTraversal(node.right, cur_ll_node)
+
+        # If one of the path is true, then we found the subpath
+        return leftPath or rightPath
+    
+
+    def validatePath(self, node, cur_ll_node):
+        if cur_ll_node is None:
+            return True # we have found our path
+        
+        if node is None:
+            return False # we reached the end of Tree
+
+        if node.val != cur_ll_node.val:
+            return False # value doesnot match - Move down the tree
+        
+        # if the value match, further move down in the sub-tree
+        leftSubTree = self.validatePath(node.left, cur_ll_node.next)
+        rightSubTree = self.validatePath(node.right, cur_ll_node.next)
+
+        return leftSubTree or rightSubTree
+    
+
+
