9a1ddb81df71ccface3a6fd08fb5c07b3bd7d6c2
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-10-11 07:13:09 -0400

    Get Right Most Set Bit

--- Striver/Bit Manipulation/Algorithms/a1.py
+++ Striver/Bit Manipulation/Algorithms/a1.py
@@ -137,6 +137,37 @@ if __name__ == '__main__':
     obj.bitManipulation(num=num, i = i)
 
 
+# ------------------------ Get Right Most Set Bit ------------------------
+
+class Solution:
+    def get_rightmost_set_bit(self, n):
+        # Turn of the right most bit
+        val = n & (n - 1)
+
+        # Get th bit that was turned off
+        right_most_set_bit = val ^ n
+
+        return right_most_set_bit
+    
+    def get_rightmost_set_bit_sinlge_line(self, n):
+        '''
+        Single line code
+        '''
+        return (n & (n-1)) ^ n
+
+
+if __name__ == '__main__':
+    obj = Solution()
+
+    n = 14
+    right_most_set_bit = obj.get_rightmost_set_bit(n)
+
+    print(right_most_set_bit)
+
+    right_most_set_bit_single_line = obj.get_rightmost_set_bit_sinlge_line(n)
+    print(right_most_set_bit_single_line)
+
+
 # ------------------------ K-th Bit is Set or Not ------------------------
 
 # Similar to Get Kth Bit
