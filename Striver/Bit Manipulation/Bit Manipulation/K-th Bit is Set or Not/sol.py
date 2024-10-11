d8631f9186bfb5449427d28e7803c8d6d1de8a6c
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-10-11 07:19:20 -0400

    kth bit set

--- Striver/Bit Manipulation/Bit Manipulation/K-th Bit is Set or Not/sol.py
+++ Striver/Bit Manipulation/Bit Manipulation/K-th Bit is Set or Not/sol.py
@@ -6,3 +6,17 @@ class Solution:
         kth_bit = n & (1 << (k))
         
         return False if kth_bit is 0 else True
+
+# ------------------------------------------------------
+
+class Solution:
+    
+    #Function to check if Kth bit is set or not.
+    def checkKthBit(self, n,k):
+        n = n >> k
+        # print(n)
+        
+        if n & 1 == 1:
+            return True
+        else:
+            return False
