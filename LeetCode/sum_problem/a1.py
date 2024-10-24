699e19c806f31f8976ffc45a4644bf4da461d8e9
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-10-23 22:04:43 -0400

    Two Sum

--- LeetCode/sum_problem/a1.py
+++ LeetCode/sum_problem/a1.py
@@ -73,3 +73,32 @@ class TwoSum:
 
 # ------------------------------ Two Sum IV ------------------------------
 # yet to solve
+
+
+# ------------------------------ Two Sum Less than K ------------------------------
+class Solution:
+    def twoSumLessThanK(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+
+        if n < 2:
+            return -1
+
+        nums.sort()
+
+        left = 0
+        right = n - 1
+
+        max_sum = -1
+
+        while left < right:
+            cur_sum = nums[left] + nums[right]
+
+            if cur_sum >= k:
+                '''Reduce the sum'''
+                right -= 1
+            elif cur_sum < k:
+                '''Capture and Increase the sum'''
+                left += 1
+                max_sum = max(max_sum, cur_sum)
+        
+        return max_sum
