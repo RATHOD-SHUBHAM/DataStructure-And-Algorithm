a3746481ed21179ba0a541eee286a2329487b94d
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-11-06 09:56:18 -0500

    Painter's Partition Problem

---
+++ Striver/Binary Search/BS on answers/Painter's Partition Problem/binary.py
@@ -0,0 +1,65 @@
+from typing import List
+
+class Solution:
+    def getPainters(self, area, boards, n):
+        '''For the given area how many painters are needed'''
+        no_of_painters = 1
+        cur_area = 0
+
+        for i in range(n):
+            if cur_area + boards[i] <= area:
+                cur_area += boards[i]
+            else:
+                cur_area = boards[i]
+                no_of_painters += 1
+        
+        return no_of_painters
+
+
+    def findLargestMinDistance(self, boards:List[int], k:int)->int:
+        n = len(boards)
+
+        if n < k:
+            return -1
+        
+        # Assign range
+        left = max(boards) # each painter can pick one board
+        right = sum(boards) # one painters paints all board
+
+        min_area = -1
+
+        # each unit of a board takes 1 unit of time to paint.
+        # 1 unit area = 1 unit time
+        while left <= right:
+            area = left + (right - left) // 2
+        
+            no_of_painters = self.getPainters(area, boards, n)
+
+            if no_of_painters <= k:
+                right = area - 1
+                min_area = area
+            else:
+                left = area + 1
+        
+        return min_area
+
+
+
+if __name__ == '__main__':
+    ob = Solution()
+    
+    # Test case : 1
+    arr = [2, 1, 5, 6, 2, 3] 
+    k = 2
+
+    # Test case : 2
+    # arr = [10, 20, 30, 40]
+    # k = 2
+
+    # Test case : 3
+    # arr = [48, 90]
+    # k = 2
+
+    result = ob.findLargestMinDistance(boards = arr, k = k)
+
+    print("the area of the minimum time to get this job done: ", result)
---
+++ Striver/Binary Search/BS on answers/Painter's Partition Problem/ques.txt
@@ -0,0 +1,53 @@
+Painter's Partition Problem
+
+Problem statement
+Given an array/list of length ‘n’, where the array/list represents the boards and each element of the given array/list represents the length of each board. Some ‘k’ numbers of painters are available to paint these boards. Consider that each unit of a board takes 1 unit of time to paint.
+
+
+
+You are supposed to return the area of the minimum time to get this job done of painting all the ‘n’ boards under a constraint that any painter will only paint the continuous sections of boards.
+
+
+
+Example :
+Input: arr = [2, 1, 5, 6, 2, 3], k = 2
+
+Output: 11
+
+Explanation:
+First painter can paint boards 1 to 3 in 8 units of time and the second painter can paint boards 4-6 in 11 units of time. Thus both painters will paint all the boards in max(8,11) = 11 units of time. It can be shown that all the boards can't be painted in less than 11 units of time.
+
+
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1 :
+4 2
+10 20 30 40
+
+
+Sample Output 1 :
+60
+
+
+Explanation For Sample Input 1 :
+In this test case, we can divide the first 3 boards for one painter and the last board for the second painter.
+
+
+Sample Input 2 :
+2 2
+48 90
+
+
+Sample Output 2 :
+90
+
+
+Expected Time Complexity:
+Try to do this in O(n*log(n)).
+
+
+Constraints :
+1 <= n <= 10^5
+1 <= k <= n
+1 <= arr[i] <= 10^9
+
+Time Limit: 1 sec.
