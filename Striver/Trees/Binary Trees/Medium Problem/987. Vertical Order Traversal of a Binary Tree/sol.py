5dc9a66ac3691ffea78329471e8b5e66b4811dce
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-12-14 23:05:26 -0500

    987. Vertical Order Traversal of a Binary Tree

---
+++ Striver/Trees/Binary Trees/Medium Problem/987. Vertical Order Traversal of a Binary Tree/sol.py
@@ -0,0 +1,47 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def __init__(self):
+        self.min_column = 0
+        self.max_column = 0
+        # Group the Nodes based on their Column or verticals
+        self.column_wise_data = defaultdict(list) # Key = column , value = (row, node)
+    
+    def bfs(self, root):
+        queue = deque([(root, 0, 0)]) # Node, Row, Column
+
+        while queue:
+            node, row, column = queue.popleft()
+
+            if node is not None:
+                self.column_wise_data[column].append((row, node.val))
+                self.min_column = min(self.min_column, column)
+                self.max_column = max(self.max_column, column)
+
+                # left and right children will be at positions (row + 1, col - 1) and (row + 1, col + 1)
+                queue.append((node.left, row + 1, column - 1))
+                queue.append((node.right, row + 1, column + 1))
+
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+
+        # step 1). BFS traversal
+        self.bfs(root)
+        print(self.column_wise_data)
+
+        # step 2). Extract the values from the columnTable
+        ret = []
+        for col in range(self.min_column, self.max_column + 1):
+            # sort first by 'row', if they belong to same row then sort by 'value', in ascending order
+            temp = []
+            for row, val in sorted(self.column_wise_data[col]):
+                temp.append(val)
+            ret.append(temp)
+
+        return ret
