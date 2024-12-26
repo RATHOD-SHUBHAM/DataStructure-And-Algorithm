8e2f68c9c7d1417cb4296767cb9aa14c3c10f9fd
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-12-25 19:08:31 -0500

    binary search plus quick sort

--- Striver/Algorithms/Searching/Quick Select/Kth Smallest/a1.py
+++ Striver/Algorithms/Searching/Quick Select/Kth Smallest/a1.py
@@ -129,3 +129,67 @@ def helper(array, startIdx,endIdx,pos):
 			
 def swap(array,left,right):
 	array[left], array[right] = array[right],array[left]
+
+
+
+# ----------------------  Binary  ----------------------
+class Solution:
+    def swap(self, arr, i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+
+
+    def partition(self, arr, left, right):
+        pivotIdx = left + (right - left) // 2
+        pivotVal = arr[pivotIdx]
+
+        while left <= right:
+            leftVal = arr[left]
+
+            if leftVal >= pivotVal:
+                self.swap(arr, left, right)
+                right -= 1
+            else:
+                left += 1
+            
+        leftVal = arr[left]   
+        if leftVal < pivotVal:
+            left += 1
+            return left
+        else:
+            return left
+        
+
+    def kthSmallest(self, arr, k):
+        n = len(arr)
+
+        # Initial Hypothetical Pivot Index
+        pivot_idx = n
+        
+        left = 0
+        right = n - 1
+
+        while pivot_idx != k:
+            new_pivot_idx = self.partition(arr, left, right)
+
+            if new_pivot_idx < k:
+                left = new_pivot_idx
+            elif new_pivot_idx > k:
+                right = new_pivot_idx - 1
+            else:
+                break
+
+            pivot_idx = new_pivot_idx
+        
+        return arr[ : k]
+
+if __name__ == '__main__':
+    # arr = [10, 4, 5, 8, 6, 11, 26]
+    # k = 3
+    # arr = [10, 4, 5, 8, 6, 11, 26]
+    # k = 2
+    # arr = [21, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    # k = 5
+    arr = [21, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    k = 2
+    ob = Solution()
+    print(ob.kthSmallest(arr, k))
