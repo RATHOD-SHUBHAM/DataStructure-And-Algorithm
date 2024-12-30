9b56d5666521f3c8d09fc68814867cc5e11d38cb
RATHOD-SHUBHAM <rathsam4@gmail.com>
2024-12-30 08:16:29 -0500

    654. Maximum Binary Tree

---
+++ Striver/Trees/Binary Trees/Hard Problems/654. Maximum Binary Tree/iterative_1.py
@@ -0,0 +1,32 @@
+# Tc and Sc: O(n).
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        
+        stack_of_nodes = []
+
+        for num in nums:
+            node = TreeNode(num)
+
+            # Set the smaller nodes as the left child
+            left_node = None
+            while stack_of_nodes and stack_of_nodes[-1].val < num:
+                # node.left = stack_of_nodes.pop()
+                left_node = stack_of_nodes.pop()
+            
+            node.left = left_node
+            
+            # Set the current node as the right child
+            if stack_of_nodes:
+                # stack_of_nodes[-1] will be the max number
+                stack_of_nodes[-1].right = node
+            
+            stack_of_nodes.append(node)
+        
+        # The root of the tree is the first node added to the stack
+        return stack_of_nodes[0]
