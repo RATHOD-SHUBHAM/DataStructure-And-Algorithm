863cb1fc32b274d6bcedbe4e7c99c122823c14a0
RATHOD-SHUBHAM <rathsam4@gmail.com>
2025-01-05 07:18:07 -0500

    kth node in a tree

---
+++ Striver/Trees/Binary Trees/Hard Problems/863. All Nodes Distance K in Binary Tree/adj_list.py
@@ -0,0 +1,67 @@
+# Tc and Sc: O(n)
+
+'''
+Since we cant traverse in reverse direction, we cannot keep track of parent nodes.
+
+3 Steps:
+1. Keep track of parents.
+2. Find the target node.
+3. Grab the distance of the remaining nodes.
+'''
+# Definition for a binary tree node.
+
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from collections import defaultdict
+
+class Solution:
+    def __init__(self):
+        self.neighbors = defaultdict(list)
+    
+    def build_adjList(self, node, parent):
+        if parent != None:
+            self.neighbors[node].append(parent)
+        
+        if node.left:
+            self.neighbors[node].append(node.left)
+            self.build_adjList(node.left, node)
+        
+        if node.right:
+            self.neighbors[node].append(node.right)
+            self.build_adjList(node.right, node)
+
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        # Grab parents nodes
+        parent = None
+        self.build_adjList(root, parent)
+            
+        
+        # Step 2: find the target -> here the target node is already given
+
+        # Step 3: Mark the distance
+        op = []
+        visited = set()
+        queue = [[target, 0]] # node, distance
+        count = 0
+        
+        while count <= k:
+            q_len = len(queue)
+            count += 1
+
+            for _ in range(q_len):
+                node, dist = queue.pop(0)
+
+                visited.add(node)
+
+                if dist == k:
+                    op.append(node.val)
+                
+                for nei in self.neighbors[node]:
+                    if nei not in visited:
+                        queue.append([nei, dist + 1])
+            
+        return op
