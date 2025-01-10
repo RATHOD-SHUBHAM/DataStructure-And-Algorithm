20f13f70fefe4f84f3114afd60e68dd81ed87c3c
RATHOD-SHUBHAM <rathsam4@gmail.com>
2025-01-10 06:43:15 -0500

    Morris Traversal

---
+++ Striver/Trees/Binary Trees/Hard Problems/Morris Traversal/144. Binary Tree Preorder Traversal/sol.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        op = []
+
+        cur_node = root
+
+        # Root >> Left >> Right
+        while cur_node:
+            # If there is no left node - append root and move right
+            if not cur_node.left:
+                op.append(cur_node.val)
+                cur_node = cur_node.right
+            
+            
+            # If left child exist
+            else:
+                child_node = cur_node.left
+
+                # Grab the right most node of child node
+                while child_node.right is not None and child_node.right != cur_node:
+                    child_node = child_node.right
+                
+                # Check if there is a thread or not
+                if child_node.right == None:
+                    op.append(cur_node.val)
+                    # there is no thread - so create a thread and move left
+                    child_node.right = cur_node
+                    cur_node = cur_node.left
+                
+                else:
+                    # thread is found - break the thread
+                    child_node.right = None
+                    cur_node = cur_node.right
+
+        return op
---
+++ Striver/Trees/Binary Trees/Hard Problems/Morris Traversal/94. Binary Tree Inorder Traversal/ques.txt
@@ -0,0 +1,47 @@
+94. Binary Tree Inorder Traversal
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+Input: root = [1,null,2,3]
+
+Output: [1,3,2]
+
+Explanation:
+
+
+
+Example 2:
+
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+
+Output: [4,2,6,5,7,1,3,9,8]
+
+Explanation:
+
+
+
+Example 3:
+
+Input: root = []
+
+Output: []
+
+Example 4:
+
+Input: root = [1]
+
+Output: [1]
+
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
