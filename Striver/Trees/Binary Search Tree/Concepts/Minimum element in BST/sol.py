08c2a044d3891fcb8accee55fb7832dfc0b31f01
RATHOD-SHUBHAM <rathsam4@gmail.com>
2025-01-16 07:13:42 -0500

    Minimum element in BST

---
+++ Striver/Trees/Binary Search Tree/Concepts/Minimum element in BST/sol.py
@@ -0,0 +1,31 @@
+"""
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+"""
+
+import math
+
+class Solution:
+    #Function to find the minimum element in the given BST.
+    def __init__(self):
+        self.min_val = math.inf
+        
+    def minValue(self, root):
+        ##Your code here
+        if not root:
+            return
+        
+        queue = [root]
+        
+        while queue:
+            node = queue.pop(0)
+            self.min_val = min(node.data, self.min_val)
+            
+            if node.left:
+                queue.append(node.left)
+            
+        
+        return self.min_val
