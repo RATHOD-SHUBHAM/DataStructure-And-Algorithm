9e970afb024572722d1ca9bb77d05162a147cd1e
RATHOD-SHUBHAM <rathsam4@gmail.com>
2025-03-05 07:30:09 -0500

    105. Construct Binary Tree from Preorder and Inorder Traversal

--- Striver/Trees/Binary Trees/Hard Problems/105. Construct Binary Tree from Preorder and Inorder Traversal/sol.py
+++ Striver/Trees/Binary Trees/Hard Problems/105. Construct Binary Tree from Preorder and Inorder Traversal/sol.py
@@ -39,3 +39,43 @@ class Solution:
         root.right = self.construct(root_idx + 1, end, inorder_dict, preorder)
 
         return root
+
+
+
+
+# -------------------- Same solution but without dictionary --------------------
+"""
+Study the dictionary approach and then try to implement this
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.idx = 0
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        n = len(inorder)
+
+        start = 0
+        end = n - 1
+
+        return self.BST(start, end, preorder, inorder)
+    
+    def BST(self, start, end, preorder, inorder):
+        if start > end:
+            return 
+        
+        root_val = preorder[self.idx]
+        self.idx += 1
+
+        root = TreeNode(root_val)
+
+        root_idx = inorder.index(root_val)
+
+        root.left = self.BST(start, root_idx - 1, preorder, inorder)
+        root.right = self.BST(root_idx + 1, end, preorder, inorder)
+
+        return root
