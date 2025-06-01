9d4967edf67b7cd2c12619de6128ab5d211f9a7c
RATHOD-SHUBHAM <rathsam4@gmail.com>
2025-06-01 08:35:54 -0400

    Partitions With Given Difference

--- Striver/Dp/Dp_on_Subsequence/Partition a set into two subsets /Dp 18- Count Partitions With Given Difference/a1.py
+++ Striver/Dp/Dp_on_Subsequence/Partition a set into two subsets /Dp 18- Count Partitions With Given Difference/a1.py
@@ -6,6 +6,113 @@ from math import *
 from typing import List
 
 
+def recursion(idx, target, arr):
+    # base case
+    if idx < 0:
+        if target == 0:
+            return 1
+        else:
+            return 0
+        
+    # Logic
+    if arr[idx] <= target:
+        take = recursion(idx - 1, target - arr[idx], arr)
+    else:
+        take = 0
+    
+    no_take = recursion(idx - 1, target, arr)
+
+    return take + no_take
+
+
+
+
+def countPartitions(n: int, d: int, arr: List[int]) -> int:
+    # write your code here
+    total = sum(arr)
+
+    # base case
+    """
+    Edge Cases:
+    1. (S + D) must be even - otherwise no valid partition exists
+    2. D < S difference can't be larger than total sum
+    """
+    if d > total or (total + d) % 2 != 0:
+        return 0
+    
+    target = (d + total) // 2
+
+    idx = n - 1
+
+    MOD = (10 ** 9) + 7
+
+    return recursion(idx, target, arr) % MOD
+
+
+# ------------------------ Memoization ------------------------
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from typing import List
+
+
+def recursion(idx, memo, target, arr):
+    # base case
+    if idx < 0:
+        if target == 0:
+            return 1
+        else:
+            return 0
+    
+    if (idx, target) in memo:
+        return memo[(idx, target)]
+        
+    # Logic
+    if arr[idx] <= target:
+        take = recursion(idx - 1, memo, target - arr[idx], arr)
+    else:
+        take = 0
+    
+    no_take = recursion(idx - 1, memo, target, arr)
+
+    memo[(idx, target)] = take + no_take
+
+    return memo[(idx, target)]
+
+
+
+
+def countPartitions(n: int, d: int, arr: List[int]) -> int:
+    # write your code here
+    total = sum(arr)
+
+    # base case
+    if d > total or (total + d) % 2 != 0:
+        return 0
+    
+    target = (d + total) // 2
+
+    idx = n - 1
+
+    MOD = (10 ** 9) + 7
+
+    memo = {}
+
+    return recursion(idx, memo, target, arr) % MOD
+
+
+# ------------------------ Tabulation ------------------------
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from typing import List
+
+
 def countPartitions(n: int, d: int, arr: List[int]) -> int:
     # write your code here
     total_sum = sum(arr)
