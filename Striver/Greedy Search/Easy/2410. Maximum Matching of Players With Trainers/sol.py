bad9bcb2b2d28c49ec53606b74e09c1035aa2f01
RATHOD-SHUBHAM <rathsam4@gmail.com>
2025-06-15 19:30:22 -0400

    2410. Maximum Matching of Players With Trainers

--- Striver/Greedy Search/Easy/2410. Maximum Matching of Players With Trainers/sol.py
+++ Striver/Greedy Search/Easy/2410. Maximum Matching of Players With Trainers/sol.py
@@ -19,3 +19,27 @@ class Solution:
             p2 += 1
         
         return p1
+
+# ---------------------------- Same Solution ----------------------------
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        m = len(players)
+        n = len(trainers)
+        
+        players.sort() 
+        trainers.sort()
+
+        i = m - 1
+        j = n - 1
+
+        count = 0
+
+        while i >= 0 and j >= 0:
+            if players[i] <= trainers[j]:
+                count += 1
+                i -= 1
+                j -= 1
+            else:
+                i -= 1
+        
+        return count
