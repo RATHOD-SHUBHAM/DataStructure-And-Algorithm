f4f88a8eff759e9ba556ecc18ca4bf4300d530c9
RATHOD-SHUBHAM <rathsam4@gmail.com>
2025-07-11 07:01:37 -0400

    198. House Robber, adding space optimization

--- Striver/Dp/1D_DP/198. House Robber/a1.py
+++ Striver/Dp/1D_DP/198. House Robber/a1.py
@@ -58,18 +58,20 @@ class Solution:
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        
+
         p_prev = 0
-        cur_pick = prev = nums[0]
+        prev = 0
+        
+        for idx in range(n):
+            # Logic
+            pick = nums[idx] + p_prev
+            no_pick = prev
 
-        for i in range(1, n):
-            pick = nums[i] + (p_prev if i-2 >= 0 else 0) # the brackets are important
-            not_pick = 0 + (prev if i-1 >= 0 else 0)
-
-            cur_pick = max(pick, not_pick)
+            cur = max(pick, no_pick)
 
             p_prev = prev
-            prev = cur_pick
+            prev = cur
         
-        return cur_pick
+        return prev
+    
         
