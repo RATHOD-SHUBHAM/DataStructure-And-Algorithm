4ca86bee32f57776b8ea7545e4561d1b2f9db71c
RATHOD-SHUBHAM <rathsam4@gmail.com>
2025-08-03 18:54:38 -0400

    3159. Find Occurrences of an Element in an Array

---
+++ LeetCode/Company/JPMC/3159. Find Occurrences of an Element in an Array/sol.py
@@ -0,0 +1,31 @@
+# Tc: O(n + q) where n = length of nums, q = number of queries
+# Sc: O(n), where n = length of nums
+
+class Solution:
+    def occurrencesOfElement(self, nums: List[int], queries: List[int], x: int) -> List[int]:
+        
+        # Step 1: Preprocessing Phase: Build a map of where each number appears
+        # get the kth occurnaces og number
+        dic = collections.defaultdict(list)
+
+        n = len(nums)
+
+        for idx in range(n):
+            dic[nums[idx]].append(idx)
+
+        # Step 2: Query Phase: For each query k, lookup the kth position directly
+        # Capture the Kth occurance of x
+        m = len(dic[x])
+
+        op = []
+
+        for i in range(len(queries)):
+            k_th_occurance = queries[i]
+
+            # The boundary check if k_th occurance of x exist or not
+            if k_th_occurance > m:
+                op.append(-1)
+            else:
+                op.append(dic[x][k_th_occurance - 1])
+        
+        return op
