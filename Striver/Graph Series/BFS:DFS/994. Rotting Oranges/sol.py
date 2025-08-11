c88b967fbee2d95d31b864ebeca120b5fd8e03a3
RATHOD-SHUBHAM <rathsam4@gmail.com>
2025-08-10 21:32:56 -0400

    994. Rotting Oranges

--- Striver/Graph Series/BFS:DFS/994. Rotting Oranges/sol.py
+++ Striver/Graph Series/BFS:DFS/994. Rotting Oranges/sol.py
@@ -1,53 +1,51 @@
-# BFS matrix
+# Tc and Sc: O(m*n)
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        
-        # In order to know if there are any fresh oranges that cannot be rotten 
-        # We can see how many fresh oranges were present in beginning to how how many fresh are left out.
 
-        # initialize queue
-        queue = deque()
+        queue = collections.deque()
+        
+        # Step 1: Initial grid scanning to find fresh/rotten oranges
+        no_of_fresh_orange = 0 # Instead of reiterating the grid, this counter will help us keep track of fresh orange in the end
 
-        # [up, down, left, right]
-        directions = [[-1,0], [1,0], [0,-1], [0,1]]
-        
-        
-        # Count the number of fresh and add rotten orange to queue
-        fresh_orange = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    fresh_orange += 1         
-                
+                    no_of_fresh_orange += 1
                 elif grid[i][j] == 2:
-                    queue.append([i,j])   
-                    
-        
-        # Counter to see at what time fresh orange become 0
-        time = 0
-        
-        while queue and fresh_orange > 0:
-            
-            # Loop through all the element of that pareticular time stamp
-            for _ in range(len(queue)):
-                row, col = queue.popleft()
-                
-                for adj_row, adj_col in directions:
-                    nei_row = adj_row + row
-                    nei_col = adj_col + col
-                    
-                    if ( nei_row < 0 or nei_row >= m or nei_col < 0 or nei_col >= n 
-                        or grid[nei_row][nei_col] != 1 ):
+                    queue.append((i,j,0)) # coords and time
+        # print(queue)
+        # print(no_of_fresh_orange)
+
+        # Step 2: Multi-source BFS simulation
+        directions = [[0, 1], [0, -1],[1, 0],[-1, 0]]
+
+        min_time = 0
+
+        while queue:
+            z = len(queue) # All oranges that get rotten in the same minute are processed together.
+
+            for _ in range(z):
+                i, j , tme = queue.popleft()
+                min_time = tme
+
+                # Explore its neighbors
+                for nei in directions:
+                    nei_i = i + nei[0]
+                    nei_j = j + nei[1]
+
+                    # If empty or already rotten, then skip
+                    if nei_i < 0 or nei_j < 0 or nei_i >= m or nei_j >= n or grid[nei_i][nei_j] == 0 or grid[nei_i][nei_j] == 2:
                         continue
-                        
-                    # else
-                    grid[nei_row][nei_col] = 2
-                    queue.append([nei_row,nei_col])
                     
-                    fresh_orange -= 1
-                    
-            time += 1 # Increase time after all the oranges in the current time stamp has been rotten
-                
-        return time if fresh_orange == 0 else -1
+                    # Fresh orange found -> rot it and reduce count of fresh orange
+                    queue.append((nei_i, nei_j , tme + 1))
+                    grid[nei_i][nei_j] = 2
+
+                    no_of_fresh_orange -= 1
+        
+        # print(no_of_fresh_orange)
+
+        return min_time if no_of_fresh_orange == 0 else -1
