051b44b3a5b76ceb143ba31cb1cccc354ca85066
RATHOD-SHUBHAM <rathsam4@gmail.com>
2025-08-13 08:31:52 -0400

    1020. Number of Enclaves

--- Striver/Graph Series/BFS:DFS/1020. Number of Enclaves/sol.py
+++ Striver/Graph Series/BFS:DFS/1020. Number of Enclaves/sol.py
@@ -1,43 +1,52 @@
-'''
-    Clever Stratergy:
-        1. So bascially if we dont hit the border, then we will have a count greater than zero.
-        2. Instead of using visited array, we can convert 1 to 0.
-'''
+# Tc and Sc: O(m * n)
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
 
-        no_of_enclaves = 0
+        directions = [[0,1], [0,-1], [1,0], [-1,0]]
+
+        # Step 1: Explore boundaries for Land Cell
+        for col in range(n):
+            # First row
+            self.dfs(0, col, directions, grid, m, n)
+            
+            # Last row
+            self.dfs(m-1, col, directions, grid, m, n)
+
+        for row in range(m):
+            # First Column
+            self.dfs(row, 0, directions, grid, m, n)
+            
+            # Last Column
+            self.dfs(row, n-1, directions, grid, m, n)
+
+        
+        #Step 2: Count the land cell
+        # land = 0
+        # for i in range(m):
+        #     for j in range(n):
+        #         if grid[i][j] == 1:
+        #             land += 1
+
+        land = 0
         for i in range(m):
-            for j in range(n):
-
-                if grid[i][j] == 0:
-                    continue
-                
-                count = self.dfs(i, j, grid, m, n)
-
-                # this trick will help us skip the borders
-                if count > 0:
-                    no_of_enclaves += count
+            land += sum(grid[i])
         
-        return no_of_enclaves
-
-    def dfs(self, i, j, grid, m, n):
-        # if we hit a border -> return a bizzar large negative number
-        if i < 0 or j < 0 or i >= m or j >= n:
-            return -math.inf
+        return land
+    
+    def dfs(self, i, j, directions, grid, m, n):
+        # base case
+        if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] == 0:
+            return
         
-        if grid[i][j] == 0:
-            return 0
-        
-        # mark the cell as visited
+        # Flip 1 -> -1
         grid[i][j] = 0
 
-        up = self.dfs(i - 1, j, grid, m, n)
-        down = self.dfs(i + 1, j, grid, m, n)
-        left = self.dfs(i, j - 1, grid, m, n)
-        right = self.dfs(i, j + 1, grid, m, n)
+        for nei in directions:
+            nei_i = i + nei[0]
+            nei_j = j + nei[1]
 
-        return 1 + (up + down + left + right)
-
+            self.dfs(nei_i, nei_j, directions, grid, m, n)
+        
+        return
