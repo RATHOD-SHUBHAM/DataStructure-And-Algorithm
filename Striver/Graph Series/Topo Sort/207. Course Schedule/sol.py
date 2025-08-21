6c59e29889396d1134bdecba60ff31452669dced
RATHOD-SHUBHAM <rathsam4@gmail.com>
2025-08-20 20:34:01 -0400

    207. Course Schedule

--- Striver/Graph Series/Topo Sort/207. Course Schedule/sol.py
+++ Striver/Graph Series/Topo Sort/207. Course Schedule/sol.py
@@ -1,59 +1,47 @@
-# Kahn's Algorithm
-
-'''
-# https://leetcode.com/problems/course-schedule-ii/discuss/3541614/Kahn's-Algorithm-BFS-%2B-InDegree-*Python-Code*
-
-# Node ordering
-    bi needs to be completed before ai
-    or
-    bi needs to occur before ai
-
-    so bi will point to ai
-'''
-
 # Tc | Sc : O(V+E)
 
-from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        n = numCourses
+        # Build graph
+        graph = collections.defaultdict(list)
 
-        # Step 1: Build graph
-        graph = defaultdict(list)
-        for edge in prerequisites:
-            ai, bi = edge
-            graph[bi].append(ai)
+        for u, v in prerequisites:
+            graph[v].append(u)
+
         
-        # Step 2:  Get the indegree of nodes
-        indegree = [0] * n
-        for edge in prerequisites:
-            ai, _ = edge
-            indegree[ai] += 1
+        # Step 1: Indegree of the node
+        indegree = [0] * numCourses
+
+        for u, v in prerequisites:
+            indegree[u] += 1 
         
-        # Step 3: Add the nodes with indegree = 0 to queue.
-        queue = []
-        for i in range(n):
+        # Step 2: Get the nodes with no pre req
+        queue = collections.deque()
+
+        for i in range(numCourses):
             if indegree[i] == 0:
                 queue.append(i)
         
-        # Step 4: Topological sorting
-        topo_sort = []
+        # Step 3: Iterate and unlock the neighbors
         count = 0
+        topo_sort = []
 
         while queue:
-            node = queue.pop(0)
+            node = queue.popleft()
 
-            # Grab neighbors and reduce their indegree.
             for nei in graph[node]:
+                # Unlock the nei
                 indegree[nei] -= 1
 
+                # Check if there are no pre req left for the nei
                 if indegree[nei] == 0:
                     queue.append(nei)
-
-            topo_sort.append(node)
+            
             count += 1
+            topo_sort.append(node)
         
-        if count != n:
-            return False
-        else:
+
+        if count == numCourses:
             return True
+        else:
+            return False
