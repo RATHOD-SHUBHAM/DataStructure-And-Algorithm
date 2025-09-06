ac42db231c19b4f1513ccd4260ff7f728efccaf9
RATHOD-SHUBHAM <rathsam4@gmail.com>
2025-09-06 10:02:30 -0400

    743. Network Delay Time

--- Striver/Graph Series/Shortest Path Algorithm/743. Network Delay Time/sol.py
+++ Striver/Graph Series/Shortest Path Algorithm/743. Network Delay Time/sol.py
@@ -1,51 +1,34 @@
-# Tc: O(N + ElogV) | Sc: O(V + E)
-
-from collections import defaultdict
-import heapq
-import math
-
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        # network of n nodes, labeled from 1 to n and not 0 - n -1
-        graph = defaultdict(list)
-        time = defaultdict()
-        for i in times:
-            src , dst , cost = i
-            graph[src - 1].append(dst - 1)
-            time[(src - 1,dst - 1)] = cost
-        
-        # print(graph)
-        # print(time)
-        
-        # if the src has no other nodes then it cannot traverse the entire graph
-        if k - 1 not in graph:
-            return -1
-        
-        
-        distance = [math.inf] * n
-        distance[k - 1] = 0
+        # Build Graph
+        graph = collections.defaultdict(list)
+        for tme in times:
+            u, v, wt = tme
+            graph[u].append((v, wt))
         
         minHeap = []
         heapq.heapify(minHeap)
-        
-        heapq.heappush(minHeap, (0 , k - 1)) 
-        
+
+        heapq.heappush(minHeap, (0, k)) # time, src
+
+        dist = [math.inf] * (n+1) # 1 based index
+        dist[k] = 0
+
         while minHeap:
-            dist , node = heapq.heappop(minHeap)
-            
-            for nei in graph[node]:
-                child_cost = time[(node, nei)] # cost
-                
-                updated_cost = child_cost + dist
-                
-                if updated_cost < distance[nei]:
-                    distance[nei] = updated_cost
-                    heapq.heappush(minHeap, (updated_cost , nei))
-                    
-        # print(distance)
-        if math.inf in distance:
+            wt, node = heapq.heappop(minHeap)
+
+            for neighbor in graph[node]:
+                nei, nei_wt = neighbor
+
+                new_wt = wt + nei_wt
+
+                if new_wt < dist[nei]:
+                    dist[nei] = new_wt
+                    heapq.heappush(minHeap, (new_wt, nei)) # time, src
+        
+
+        dist = dist[1: ] # 1 based index
+        if math.inf in dist:
             return -1
         else:
-            return max(distance)
-        
-        
+            return max(dist)
