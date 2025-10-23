d9aee060dc6403c14696b9bb6cf7192e68e37ab9
RATHOD-SHUBHAM <rathsam4@gmail.com>
2025-10-23 08:32:38 -0400

    219. Contains Duplicate II

--- NeetCode/Array/Contains Duplicate/219. Contains Duplicate II/sol.py
+++ NeetCode/Array/Contains Duplicate/219. Contains Duplicate II/sol.py
@@ -1,10 +1,37 @@
-# tc and Sc: O(n)
+# Tc and Sc: O(n)
+
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        dic = {}
-        
+        dic = collections.defaultdict(int)
+
         for i in range(len(nums)):
-            if nums[i] in dic and (abs(dic[nums[i]] - i) <= k):
-                return True
-            dic[nums[i]] = i
+            if nums[i] in dic:
+                j = dic[nums[i]]
+                diff = abs(i - j)
+
+                if diff <= k:
+                    return True
+                else:
+                    dic[nums[i]] = i
+            else:
+                dic[nums[i]] = i
+        
+        return False
+    
+# -------------------------------- Clean Code --------------------------------
+# Tc and Sc: O(n)
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        dic = collections.defaultdict(int)
+
+        for i in range(len(nums)):
+            if nums[i] in dic:
+                j = dic[nums[i]]
+                diff = abs(i - j)
+
+                if diff <= k:
+                    return True
+            
+            dic[nums[i]] = i # Replace the number's index with the latest index
+        
         return False
