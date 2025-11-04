86670db679e4c487d5596504029d44bab352b976
RATHOD-SHUBHAM <rathsam4@gmail.com>
2025-11-04 09:55:44 -0500

    Two Sum

---
+++ NeetCode/NeetCode_150/Arrays_&_Hashing/1. Two Sum/sol.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+
+        dic = {}
+
+        for i in range(n):
+            diff = target - nums[i]
+
+            if diff in dic:
+                return [i, dic[diff]]
+            
+            dic[nums[i]] = i
+
