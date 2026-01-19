89960dd803f0bfae19ccb8c921364adb0949c465
SHUBHAM SHANKAR <58945964+RATHOD-SHUBHAM@users.noreply.github.com>
2026-01-19 12:06:33 -0500

    128. Longest Consecutive Sequence
    Use set to check if a number is present in current list

---
+++ NeetCode/NeetCode_150/Arrays_&_Hashing/128. Longest Consecutive Sequence/sol.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        
+        n = len(nums)
+
+        LCS = 0
+
+        for i in range(n):
+            cur_num = nums[i]
+
+            if cur_num - 1 in num_set:
+                continue
+            
+            count = 0
+            while cur_num in num_set:
+                count += 1
+                cur_num += 1
+            
+            LCS = max(LCS, count)
+        
+        return LCS
