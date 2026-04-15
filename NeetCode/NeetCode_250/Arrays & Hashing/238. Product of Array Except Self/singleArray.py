88f7ed67c7946c8fe21884b1e8d3292d0345c857
SHUBHAM SHANKAR <58945964+RATHOD-SHUBHAM@users.noreply.github.com>
2026-04-14 20:01:29 -0400

    238. Product of Array Except Self

--- NeetCode/NeetCode_250/Arrays & Hashing/238. Product of Array Except Self/singleArray.py
+++ NeetCode/NeetCode_250/Arrays & Hashing/238. Product of Array Except Self/singleArray.py
@@ -4,16 +4,13 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
 
-        # Left Running Sum
-        prod = [1] * n
+        output = [1] * n
         for i in range(1, n):
-            prod[i] = prod[i - 1] * nums[i-1]
+            output[i] = output[i-1] * nums[i-1]
         
-        # Right Running Sum
         right_prod = 1
         for i in reversed(range(n-1)):
             right_prod = right_prod * nums[i+1]
-            prod[i] = prod[i] * right_prod
+            output[i] = right_prod * output[i]
         
-
-        return prod
+        return output
