f1881e3b2ca56f1e88182c42d0e0d711cc088b1a
SHUBHAM SHANKAR <58945964+RATHOD-SHUBHAM@users.noreply.github.com>
2026-06-21 13:07:15 -0400

    k_sum

--- NeetCode/NeetCode_250/Two Pointers/18. 4Sum/k_sum.py
+++ NeetCode/NeetCode_250/Two Pointers/18. 4Sum/k_sum.py
@@ -97,7 +97,7 @@ class Solution:
         return res
     
 
-# ========================== Clean Solution ==========================
+# ======================================= Clean Solution =======================================
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
