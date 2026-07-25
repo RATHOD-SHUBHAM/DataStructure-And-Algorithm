af3fbf10b0dc69afca4e1e83541b44e74bf99519
SHUBHAM SHANKAR <58945964+RATHOD-SHUBHAM@users.noreply.github.com>
2026-07-25 12:30:23 -0400

    Duplicate check in current window

--- NeetCode/NeetCode_250/Sliding Window/3. Longest Substring Without Repeating Characters/sol.py
+++ NeetCode/NeetCode_250/Sliding Window/3. Longest Substring Without Repeating Characters/sol.py
@@ -48,7 +48,8 @@ class Solution:
         while right < n:
 
             # Check if dic[s[right]] is part of current window and if new left is after the old left, because we cannot be going back
-            if s[right] in dic and dic[s[right]] >= left:
+            # eg: "tmmzuxt"
+            if s[right] in dic and dic[s[right]] >= left: # Check if there are duplicate in current window
                 left = dic[s[right]] + 1
             
             cur_dist = (right - left) + 1
