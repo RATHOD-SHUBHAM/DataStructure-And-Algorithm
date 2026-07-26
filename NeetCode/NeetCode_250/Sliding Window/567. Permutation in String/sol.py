25293144c3c3e6014bce6b8c40d56074a05814e3
SHUBHAM SHANKAR <58945964+RATHOD-SHUBHAM@users.noreply.github.com>
2026-07-26 12:49:13 -0400

    567. Permutation in String

---
+++ NeetCode/NeetCode_250/Sliding Window/567. Permutation in String/sol.py
@@ -0,0 +1,60 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        m = len(s1)
+        n = len(s2)
+
+        if m > n:
+            return False
+        
+        left = 0
+        right = m - 1
+
+        need = collections.Counter(s1)
+
+        while right < n:
+            # Create a window of size m for s2 and check if it is a permutation of s1
+            ele = s2[left : right + 1] # rebuild the array => o(m*n)
+
+            if collections.Counter(ele) == need:
+                return True
+            
+            else:
+                left += 1
+                right += 1
+        
+        return False
+
+# ===================================================================
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n1 = len(s1)
+        n2 = len(s2)
+
+        if n1 > n2:
+            return False
+        
+        s1_count = [0] * 26
+        s2_count = [0] * 26
+
+        for i in range(n1):
+            idx1 = ord(s1[i]) - ord('a')
+            idx2 = ord(s2[i]) - ord('a')
+
+            s1_count[idx1] += 1
+            s2_count[idx2] += 1
+        
+        if s1_count == s2_count:
+            return True
+        
+        for i in range(n1,  n2):
+            left_idx = ord(s2[i - n1]) - ord('a')
+            s2_count[left_idx] -= 1
+
+            right_idx = ord(s2[i]) - ord('a')
+            s2_count[right_idx] += 1
+
+            if s1_count == s2_count:
+                return True
+
+        return False
